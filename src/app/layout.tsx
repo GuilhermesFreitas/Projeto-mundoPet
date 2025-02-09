b447275bba17a49480c356d21b730ae907e42cf9
guilherme freitas <guilhermefreitasea@gmail.com>
2025-02-09 15:23:11 -0300

    fix deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,27 @@
-import { Link } from "lucide-react";
+import { Metadata } from "next";
+import { ReactNode } from "react";
 import "./globals.css";
 
-export const generateMetadata = (title: string, description: string) => {
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-    },
-    robots: {
-      index: true,
-      follow: true,
-      nocache: true,
-      googleBot: {
-        index: true,
-        follow: true,
-        nocache: true,
-        noimageindex: true,
-      },
-    },
-  };
+export const metadata: Metadata = {
+  title: "MundoPet",
+  description: "O melhor lugar para o seu pet",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+
+export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="pt-BR">
       <head>
-        <link 
+        <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className="font-lato antialiased">
-        {children}
-      </body>
+      <body className="font-lato antialiased">{children}</body>
     </html>
   );
 }
